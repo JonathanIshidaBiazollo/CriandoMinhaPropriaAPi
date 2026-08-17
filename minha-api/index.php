b2bf6e83340718804e1e61d110c254431eaf7337
JonathanIshidaBiazollo <jonathan.i.b@hotmail.com>
2026-08-17 16:17:27 -0300

    Refactor index.php for user management functionality

--- minha-api/index.php
+++ minha-api/index.php
@@ -11,43 +11,114 @@
       <input type="email" id="email">
       <button id="cadastrar">Cadastrar</button>
     </form>
-    <button id="listar">Listar</button>
+    <button id="buscar">Listar</button>
     <ul id="lista"></ul>
     <script>
-     const lista = document.querySelector("#lista");
-     const listar = document.querySelector("#listar");
-     listar.addEventListener("click", async()=>{
-      const resposta = await fetch("http://localhost/projetos/minha-api/usuarios.php");
-      const usuarios = await resposta.json();
-      console.log(usuarios);
-      lista.innerHTML = "";
-      usuarios.forEach(usuario=>{
-        lista.innerHTML+=`
-          <li data-id="${usuario.id}">
-            <p>${usuario.nome}</p>
-            <p>${usuario.email}</p>
-            <button data-id="${usuario.id}" class="excluir">excluir</button>
-            <button data-id="${usuario.id}" class="editar">Editar</button>
-          </li>
-        `;
-      })
-     });
+      const API_URL = "http://localhost/projetos/minha-api/usuarios.php";
 
-    lista.addEventListener("click", async(evento)=>{
-      if(evento.target.classList.contains("excluir")){
-        const id = evento.target.dataset.id;
-        console.log(id);
-        const resposta = await fetch(`http://localhost/projetos/minha-api/usuarios.php/${id}`, {
-          method: "DELETE"
-        });
-        evento.target.parentElement.remove();
-        console.log(resposta);
-      }else if(evento.target){
-        alert("Ainda não criei o evento de editar")
-      }
-      
-      const cadastrar = document.querySelector("#cadastrar");
-    });
+      //Listar todos os usuários
+      const lista = document.querySelector("#lista");
+      const botao = document.querySelector("#buscar");
+
+      botao.addEventListener("click", async()=>{
+        try{
+          const resposta = await fetch(API_URL);
+
+          if(!resposta.ok){
+            throw new Error("Erro ao buscar usuários");
+          }
+
+          const usuarios = await resposta.json();
+          console.log(usuarios);
+
+          lista.innerHTML = "";
+          usuarios.forEach(usuario => {
+            lista.innerHTML+=`
+              <li data-id="${usuario.id}">
+                <h2>${usuario.nome}</h2>
+                <p>${usuario.email}</p>
+                <button data-id="${usuario.id}" class="excluir">excluir</button>
+                <button data-id="${usuario.id}" class="editar">Editar</button>
+              </li>
+            `;
+          })
+        }catch(erro){
+          console.error(erro);
+          lista.innerHTML = `
+            <li>Erro ao carregar usuários</li>
+          `;
+        }
+      });
+
+      //Adicionar mais usuários
+      const formulario = document.querySelector("#formulario");
+
+      formulario.addEventListener("submit", async(evento) => {
+        evento.preventDefault();//Por padrão o botão de submit envia o formulário e nesse caso eu quero evitar isso
+
+        const nome = document.querySelector("#nome").value;
+        const email = document.querySelector("#email").value;
+
+        try{
+          const resposta = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({//bem parecido com a estrutura que se usa em testes no POSTMAN, porém aqui é necessário fazer a conversão, coisa que o POSTMAN faria por mim automaticamente
+              nome: nome,
+              email: email
+            })
+          });
+
+          if(!resposta.ok){
+            throw new Error("Erro ao cadastrar usuário");
+          }
+
+          const dados = await resposta.json();
+          lista.innerHTML += `
+            <li data-id="${dados.id}">
+              <h2>${dados.nome}</h2>
+              <p>${dados.email}</p>
+              <button data-id="${dados.id}" class="excluir">Excluir</button>
+              <button data-id="${dados.id}" class="editar">Editar</button>
+            </li>
+          `;
+          //Com essa inserção do html, não éw necessário fazer outro get pra atualizar a lista, pois o POST já devolveu os dados que foram cadastrados a pouco, então é só usá-los diretamente
+          console.log(dados);
+          alert("Usuário cadastrado com sucesso!");
+          formulario.reset();
+        }catch(erro){
+          console.error(erro);
+          alert("Erro ao cadastrar usuário");
+        }
+      });
+
+      //Apagar usuários
+      lista.addEventListener("click", async(evento) => {
+        if(evento.target.classList.contains("excluir")){
+          const id = evento.target.dataset.id;
+          console.log("ID a ser excluído: ", id);
+          try{
+            const resposta = await fetch(`${API_URL}?id=${id}`, {
+              method: "DELETE"
+            });
+            if(!resposta.ok){
+              throw new Error("Erro ao excluir usuário");
+            }
+
+            const dados = await resposta.json();
+            console.log(dados);
+            evento.target.parentElement.remove();
+          }catch(erro){
+            console.error(erro);
+            alert("Erro ao excluir usuário");
+          }
+          
+        }
+      });
+
     </script>
   </body>
 </html>
