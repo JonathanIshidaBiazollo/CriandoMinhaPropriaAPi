9d00a85eab2575470be2f9f930b35781e890b43d
JonathanIshidaBiazollo <jonathan.i.b@hotmail.com>
2026-08-17 16:17:49 -0300

    Enhance user creation and update with validation
    Added validation for name and email in POST and PATCH methods. Improved response codes for successful and error cases.

--- minha-api/usuarios.php
+++ minha-api/usuarios.php
@@ -3,15 +3,27 @@
     require_once "conexao.php";
     $metodo = $_SERVER["REQUEST_METHOD"];//Informa se a requisição é GET, POST, PUT, PATCH ou DELETE
     if($metodo === "GET"){
+        //BUSCAR
         $sql = "SELECT * FROM usuarios";
         $stmt = $pdo->query($sql);
         $usuarios = $stmt->fetchALL(PDO::FETCH_ASSOC);
         echo json_encode($usuarios);
     }else if($metodo === "POST"){
+        //CADASTRAR
         $dados = json_decode(
             file_get_contents("php://input"), 
             true
         );
+
+        if(empty($dados["nome"]) || empty($dados["email"])){
+            http_response_code(400);
+
+            echo json_encode([
+                "erro" => "Nome e email são obrigatórios"
+            ]);
+
+            exit;
+        }
 
         $nome = $dados["nome"];
         $email = $dados["email"];
@@ -38,10 +50,17 @@
             $nome,
             $email
         ]);
+
+        http_response_code(201);//A requisição foi criada e um novo recurso foi criado
+
         echo json_encode([
             "mensagem" => "Usuário cadastrado com sucesso",
-            "id" => $pdo->lastInsertId()
+            "id" => $pdo->lastInsertId(),
+            "nome" => $nome,
+            "email" => $email
         ]);
+
+        
         /*
         //Para usar no POSTMAN
         $stmt->execute([
@@ -55,12 +74,23 @@
         ]);
         */
     }else if($metodo === "PATCH"){
+        //ATUALIZAR/EDITAR
         $id = $_GET["id"];
 
         $dados = json_decode(
             file_get_contents("php://input"),
             true
         );
+
+        if(empty($dados["nome"]) || empty($dados["email"])){
+            http_response_code(400);
+
+            echo json_encode([
+                "erro" => "Nome e email são obrigatórios"
+            ]);
+
+            exit;
+        }
 
         $nome = $dados["nome"];
         $email = $dados["email"];
@@ -85,6 +115,7 @@
             "mensagem" => "Usuário atualizado com sucesso"
         ]);
     }else if($metodo === "DELETE"){
+        //EXCLUIR/APAGAR
         $id = $_GET["id"];
 
         $sql = "DELETE FROM usuarios
@@ -98,6 +129,12 @@
 
         echo json_encode([
             "mensagem" => "Usuário deletado com sucesso"
+        ]);
+    }else{
+        http_response_code(405);
+
+        echo_json_encode([
+            "erro" => "Método não permitido"
         ]);
     }
 ?>
